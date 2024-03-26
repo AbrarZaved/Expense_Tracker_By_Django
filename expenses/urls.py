a809d29717a062dd38ed72b105792f14f42df065
AbrarZaved <abrarzaved2002@gmail.com>
2024-03-26 18:08:17 +0600

    Add delete and edit expense functionality

--- expenses/urls.py
+++ expenses/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_expense', views.add_expense, name='expense'),
+    path('delete_expense/<int:id>', views.delete_expense, name='delete'),
+    path('edit_expense/<int:id>', views.edit_expense, name='edit')
     
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ExpenseForm
 from .models import Add_Expense
+from django.contrib import messages as message
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 # Create your views here.
 @login_required(login_url='/authentication/')
 def index(request):
+    
     data = Add_Expense.objects.all()
-    return render(request, 'expenses/index.html', {'data': data})
+    paginator = Paginator(data, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'expenses/index.html', {'data': data, 'page_obj': page_obj})
 
 def add_expense(request):
     form = ExpenseForm()
@@ -16,3 +23,20 @@ def add_expense(request):
             form.save()
             return render(request, 'expenses/add_expense.html', {'form': form})
     return render(request, 'expenses/add_expense.html', {'form': form})
+
+def delete_expense(request, id):
+    data = Add_Expense.objects.get(pk=id)
+    data.delete()
+    message.success(request, 'Expense deleted successfully')
+    return redirect('index')
+
+def edit_expense(request, id):
+    data = Add_Expense.objects.get(pk=id)
+    form = ExpenseForm(instance=data)
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=data)
+        if form.is_valid():
+            form.save()
+            message.success(request, 'Expense updated successfully')
+            return redirect('index')
+    return render(request, 'expenses/edit_expense.html', {'form': form})
