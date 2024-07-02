821070e07f5e1ef1be7b60b23ac390b0b7e5db9d
AbrarZaved <abrarzaved2002@gmail.com>
2024-07-03 02:09:05 +0600

    formatted

--- Expense_Tracker/static/js/register.js
+++ Expense_Tracker/static/js/register.js
@@ -1,83 +1,79 @@
-const usernameField = document.querySelector('#usernameField');
-const emailFeild = document.querySelector('#emailFeild');
-const feedbackArea = document.querySelector('.invalid-feedback');
-const emailFeedBackArea = document.querySelector('.emailFeedBackArea');
-const showPassword = document.querySelector('.showPassword');
-const passwordField = document.querySelector('#passwordField');
-const submitBtn = document.querySelector('.submit-btn');
-const passwordlen = document.querySelector('#passwordField');
-const passwordlength = document.querySelector('.passwordlen');
+const usernameField = document.querySelector("#usernameField");
+const emailFeild = document.querySelector("#emailFeild");
+const feedbackArea = document.querySelector(".invalid-feedback");
+const emailFeedBackArea = document.querySelector(".emailFeedBackArea");
+const showPassword = document.querySelector(".showPassword");
+const passwordField = document.querySelector("#passwordField");
+const submitBtn = document.querySelector(".submit-btn");
+const passwordlen = document.querySelector("#passwordField");
+const passwordlength = document.querySelector(".passwordlen");
 
-passwordlen.addEventListener('keyup', (e) => {
-    passwordlength.style.display = 'none';
-    const passwordVal = e.target.value;
-    if (passwordVal.length < 6 && passwordVal.length > 0) {
-        passwordlength.style.display = 'block';
-        passwordlength.innerHTML = `<p style="font-size: 12px;">Password must be at least 6 characters</p>`;
-    }
+passwordlen.addEventListener("keyup", (e) => {
+  passwordlength.style.display = "none";
+  const passwordVal = e.target.value;
+  if (passwordVal.length < 6 && passwordVal.length > 0) {
+    passwordlength.style.display = "block";
+    passwordlength.innerHTML = `<p style="font-size: 12px;">Password must be at least 6 characters</p>`;
+  }
 });
 
-
-
-showPassword.addEventListener('click', (e) => {
-    if (showPassword.textContent === 'Show Password') {
-        showPassword.textContent = 'Hide Password';
-        passwordField.setAttribute('type', 'text');
-    } else {
-        showPassword.textContent = 'Show Password';
-        passwordField.setAttribute('type', 'password');
-    }
-
-
+showPassword.addEventListener("click", (e) => {
+  if (showPassword.textContent === "Show Password") {
+    showPassword.textContent = "Hide Password";
+    passwordField.setAttribute("type", "text");
+  } else {
+    showPassword.textContent = "Show Password";
+    passwordField.setAttribute("type", "password");
+  }
 });
 
-usernameField.addEventListener('keyup', (e) => {
-    console.log('88',88);
-    const usernameVal = e.target.value;
-    usernameField.classList.remove('is-invalid');
-    feedbackArea.style.display = 'none';
-    if (usernameVal.length > 0) {
-        fetch('/authentication/validate_username', {
-            body: JSON.stringify({username: usernameVal}),
-            method: "POST",
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log("data", data);
-            if (data.username_error) {
-                submitBtn.disabled = true;
-                usernameField.classList.add('is-invalid');
-                feedbackArea.style.display = 'block';
-                feedbackArea.innerHTML = `<p style="font-size: 9px;">${data.username_error}</p>`;
-            } else {
-                submitBtn.removeAttribute('disabled');
-            }
-        });
-    }
+usernameField.addEventListener("keyup", (e) => {
+  console.log("88", 88);
+  const usernameVal = e.target.value;
+  usernameField.classList.remove("is-invalid");
+  feedbackArea.style.display = "none";
+  if (usernameVal.length > 0) {
+    fetch("/authentication/validate_username", {
+      body: JSON.stringify({ username: usernameVal }),
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        if (data.username_error) {
+          submitBtn.disabled = true;
+          usernameField.classList.add("is-invalid");
+          feedbackArea.style.display = "block";
+          feedbackArea.innerHTML = `<p style="font-size: 9px;">${data.username_error}</p>`;
+        } else {
+          submitBtn.removeAttribute("disabled");
+        }
+      });
+  }
 });
 
 emailFeild.addEventListener("keyup", (f) => {
-    const emailVal = f.target.value;
-    console.log('88',88);
-    
-    emailFeild.classList.remove("is-invalid");
-    emailFeedBackArea.style.display = "none";
-    if (emailVal.length > 0) {
-        fetch('/authentication/validate-email', {
-            body: JSON.stringify({email: emailVal}),
-            method: "POST",
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log("data", data);
-            if (data.email_error) {
-                submitBtn.disabled = true;
-                emailFeild.classList.add('is-invalid');
-                emailFeedBackArea.style.display = 'block';
-                emailFeedBackArea.innerHTML = `<p style="font-size: 12px;">${data.email_error}</p>`;
-            } else {
-                submitBtn.removeAttribute('disabled');
-            }   
-        });
-    }
+  const emailVal = f.target.value;
+  console.log("88", 88);
+
+  emailFeild.classList.remove("is-invalid");
+  emailFeedBackArea.style.display = "none";
+  if (emailVal.length > 0) {
+    fetch("/authentication/validate-email", {
+      body: JSON.stringify({ email: emailVal }),
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        if (data.email_error) {
+          submitBtn.disabled = true;
+          emailFeild.classList.add("is-invalid");
+          emailFeedBackArea.style.display = "block";
+          emailFeedBackArea.innerHTML = `<p style="font-size: 12px;">${data.email_error}</p>`;
+        } else {
+          submitBtn.removeAttribute("disabled");
+        }
+      });
+  }
 });
